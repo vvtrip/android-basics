4b37a672549b05d5209fec1c1fc21af1190a6284
vvtrip <vaibhavvtrip@gmail.com>
2018-05-14 01:37:33 +0530

    added Checkboxes and background

--- Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -14,13 +16,16 @@ public class MainActivity extends AppCompatActivity {
     RadioButton q1c3;
     RadioButton q2c2;
     RadioButton q3c3;
-    RadioButton q4c4;
+    CheckBox q4c1;
+    CheckBox q4c2;
+    CheckBox q4c3;
+    CheckBox q4c4;
     RadioButton q5c1;
     RadioButton q6c2;
     RadioButton q7c2;
     RadioButton q8c1;
     RadioButton q9c3;
-    RadioButton q10c4;
+    EditText q10a;
 
 
     @Override
@@ -31,17 +36,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submit(View view) {
-        int answer1_score;
-        int answer2_score;
-        int answer3_score;
-        int answer4_score;
-        int answer5_score;
-        int answer6_score;
-        int answer7_score;
-        int answer8_score;
-        int answer9_score;
-        int answer10_score;
-        int final_score;
+        int answer1_score = 0;
+        int answer2_score = 0;
+        int answer3_score = 0;
+        int answer4_score = 0;
+        int answer5_score = 0;
+        int answer6_score = 0;
+        int answer7_score = 0;
+        int answer8_score = 0;
+        int answer9_score = 0;
+        int answer10_score = 0;
+        int final_score = 0;
         String result;
 
         //question 1
@@ -51,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
         answer1 = q1c3.isChecked();
         if (answer1) {
             answer1_score = 1;
-        } else {
-            answer1_score = 0;
         }
 
         //question 2
@@ -62,8 +65,6 @@ public class MainActivity extends AppCompatActivity {
         answer2 = q2c2.isChecked();
         if (answer2) {
             answer2_score = 1;
-        } else {
-            answer2_score = 0;
         }
 
 
@@ -74,19 +75,25 @@ public class MainActivity extends AppCompatActivity {
         answer3 = q3c3.isChecked();
         if (answer3) {
             answer3_score = 1;
-        } else {
-            answer3_score = 0;
         }
 
         //question 4
-        Boolean answer4;
+        Boolean answer4c1;
+        Boolean answer4c2;
+        Boolean answer4c3;
+        Boolean answer4c4;
 
-        q4c4 = (RadioButton) this.findViewById(R.id.q4c4);
-        answer4 = q4c4.isChecked();
-        if (answer4) {
+        q4c1 = (CheckBox) this.findViewById(R.id.q4c1);
+        q4c2 = (CheckBox) this.findViewById(R.id.q4c2);
+        q4c3 = (CheckBox) this.findViewById(R.id.q4c3);
+        q4c4 = (CheckBox) this.findViewById(R.id.q4c4);
+
+        answer4c1 = q4c1.isChecked();
+        answer4c2 = q4c2.isChecked();
+        answer4c3 = q4c3.isChecked();
+        answer4c4 = q4c4.isChecked();
+        if (answer4c1 && answer4c2 && answer4c3 && !answer4c4) {
             answer4_score = 1;
-        } else {
-            answer4_score = 0;
         }
 
         //question 5
@@ -96,8 +103,6 @@ public class MainActivity extends AppCompatActivity {
         answer5 = q5c1.isChecked();
         if (answer5) {
             answer5_score = 1;
-        } else {
-            answer5_score = 0;
         }
 
         //question 6
@@ -107,8 +112,6 @@ public class MainActivity extends AppCompatActivity {
         answer6 = q6c2.isChecked();
         if (answer6) {
             answer6_score = 1;
-        } else {
-            answer6_score = 0;
         }
 
         //question 7
@@ -118,8 +121,6 @@ public class MainActivity extends AppCompatActivity {
         answer7 = q7c2.isChecked();
         if (answer7) {
             answer7_score = 1;
-        } else {
-            answer7_score = 0;
         }
 
         //question 8
@@ -129,8 +130,6 @@ public class MainActivity extends AppCompatActivity {
         answer8 = q8c1.isChecked();
         if (answer8) {
             answer8_score = 1;
-        } else {
-            answer8_score = 0;
         }
 
         //question 9
@@ -140,19 +139,15 @@ public class MainActivity extends AppCompatActivity {
         answer9 = q9c3.isChecked();
         if (answer9) {
             answer9_score = 1;
-        } else {
-            answer9_score = 0;
         }
 
         //question 10
-        Boolean answer10;
+        String answer10;
 
-        q10c4 = (RadioButton) this.findViewById(R.id.q10c4);
-        answer10 = q10c4.isChecked();
-        if (answer10) {
+        q10a = (EditText) this.findViewById(R.id.q10a);
+        answer10 = q10a.getText().toString();
+        if (answer10.equals("+91")) {
             answer10_score = 1;
-        } else {
-            answer10_score = 0;
         }
 
 
